b5a0dbc79284e3b5625887986f101bda73e99272
Danzo7 <aymenmiro@gmail.com>
2021-09-29 23:13:46 +0100

    add sliver support

--- lib/components/screens/home/homeScreen.dart
+++ lib/components/screens/home/homeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:minbar_fl/components/widgets/IconBuilder.dart';
 import 'package:minbar_fl/components/static/colors.dart';
-import 'package:minbar_fl/components/static/staticValues.dart';
 import 'package:minbar_fl/components/static/textStyles.dart';
 import 'package:minbar_fl/components/widgets/BroadcastBox.dart';
 import 'package:minbar_fl/components/widgets/NavgationBar/NavigationBar.dart';
@@ -9,6 +8,7 @@ import 'package:minbar_fl/components/widgets/ScrollView.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 import '../../widgets/ChipsTag.dart';
+import '../profile/ProfileScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -84,52 +84,63 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         bottom: false,
         child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Text("الاكثر تفاعلا", style: DTextStyle.bg20s),
-                alignment: Alignment.center,
-                padding: EdgeInsets.symmetric(vertical: 5),
-              ),
-              CarouselSlider(
-                  options: CarouselOptions(
-                      enableInfiniteScroll: false,
-                      height: 116.0,
-                      reverse: true),
-                  items: mediaItems
-                      .map((e) => Container(height: 113, width: 265, child: e))
-                      .toList()),
-              SizedBox(
-                height: 20,
-              ),
-              ChipsTag(items: ["الكل", "ختبة", "درس", "حوار"]),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    const Text("يبث الان", style: DTextStyle.bg20s),
-                    const IconBuilder("live", color: DColors.sadRed),
-                  ],
+          child: CustomScrollView(
+            physics: RangeMaintainingScrollPhysics(),
+            slivers: [
+              SliverToBoxAdapter(
+                  child: Wrap(spacing: 10, children: [
+                Container(
+                  child: Text("الاكثر تفاعلا", style: DTextStyle.bg20s),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 5),
                 ),
-                height: 70,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 33),
-                  child: const SpacedScrollView(
-                    children: mediaItems,
-                    spacing: 10,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: CarouselSlider(
+                      options: CarouselOptions(
+                          enableInfiniteScroll: false,
+                          height: 116.0,
+                          reverse: true),
+                      items: mediaItems
+                          .map((e) =>
+                              Container(height: 113, width: 265, child: e))
+                          .toList()),
+                ),
+              ])),
+              SliverPersistentHeader(
+                  pinned: true,
+                  floating: false,
+                  delegate: StickyShipTag(
+                    child: ChipsTag(items: ["الكل", "ختبة", "درس", "حوار"]),
+                    minHeight: 47,
+                    maxHeight: 47,
+                  )),
+              SliverToBoxAdapter(
+                child: Container(
+                  child: Column(
+                    children: [
+                      const Text("يبث الان", style: DTextStyle.bg20s),
+                      const IconBuilder("live", color: DColors.sadRed),
+                    ],
                   ),
+                  height: 70,
                 ),
               ),
+              SliverPadding(
+                padding: EdgeInsets.only(bottom: 80),
+                sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (context, index) => Padding(
+                            padding: const EdgeInsets.only(
+                                left: 33, bottom: 10, right: 33),
+                            child: mediaItems[index]),
+                        childCount: mediaItems.length)),
+              )
             ],
           ),
         ),
       ),
-      bottomNavigationBar: StaticValues.navBar,
+      bottomNavigationBar: const NavigationBar(selectedIndex: 0),
     );
   }
 }
