4d0ecb72e70518c7c9eab314357dc97f1087cf98
Danzo7 <aymenmiro@gmail.com>
2021-10-06 15:57:04 +0100

    add carousel

--- lib/components/screens/general/general_screen.dart
+++ lib/components/screens/general/general_screen.dart
@@ -1,9 +1,14 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:minbar_fl/components/static/default_colors.dart';
+import 'package:minbar_fl/components/static/default_text_styles.dart';
 import 'package:minbar_fl/components/static/fake_data.dart';
+import 'package:minbar_fl/components/widgets/broadcast_box.dart';
 import 'package:minbar_fl/components/widgets/misc/minbar_scaffold.dart';
 import 'package:minbar_fl/components/widgets/post/post.dart';
+import 'package:minbar_fl/components/widgets/slivers/sliver_header_carousel.dart';
 import 'package:minbar_fl/components/widgets/slivers/sticky_chips_tag.dart';
+import 'package:snap_scroll_physics/snap_scroll_physics.dart';
 
 class GeneralScreen extends StatelessWidget {
   final component;
@@ -12,24 +17,62 @@ class GeneralScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MinbarScaffold(
-      selectedIndex: 1,
-      body: CustomScrollView(
-        slivers: [
-          StickyChipTag(
-            items: FakeData.fields,
-            bgColor: DColors.white,
+        selectedIndex: 1,
+        body: RefreshIndicator(
+          edgeOffset: 173 + 47,
+          displacement: 100,
+          triggerMode: RefreshIndicatorTriggerMode.anywhere,
+          onRefresh: () => Future.delayed(Duration(seconds: 1), () => {true}),
+          child: CustomScrollView(
+            physics: SnapScrollPhysics(parent: BouncingScrollPhysics(), snaps: [
+              Snap(173,
+                  distance:
+                      50), // If the scroll offset is expected to stop between 150-250 the scroll will snap to 200,
+              Snap(173,
+                  leadingDistance:
+                      50), // If the scroll offset is expected to stop  between 150-200 the scroll will snap to 200,
+              Snap(173,
+                  trailingDistance:
+                      50), // If the scroll offset is expected to stop between 150-200 the scroll will snap to 200,
+              Snap(173,
+                  trailingDistance:
+                      50), // If the scroll offset is expected to stop between 150-200 the scroll will snap to 200,
+              Snap.avoidZone(0,
+                  173), // If the scroll offset is expected to stop between 0-200, the scroll will snap to 0 if the expected one is between 0-99, and to 200 if it is between 100-200,
+              Snap.avoidZone(0, 173,
+                  delimiter:
+                      50), // If the scroll offset is expected to stop between 0-200, the scroll will snap to 0 if the expected one is between 0-49, and to 200 if it is between 50-200
+            ]),
+            slivers: [
+              StickyChipTag(
+                items: FakeData.fields,
+                bgColor: DColors.white,
+              ),
+              SliverHeaderCarousel(
+                carousel: CarouselSlider(
+                    options: CarouselOptions(
+                        enableInfiniteScroll: false,
+                        height: 116.0,
+                        reverse: true),
+                    items: FakeData.casts
+                        .map((e) => Container(
+                            height: 113, width: 265, child: BroadcastBox(e)))
+                        .toList()),
+                title: Text("الاكثر تفاعلا", style: DTextStyle.bg20s),
+                minHeight: 0,
+                maxHeight: 173,
+              ),
+              SliverPadding(
+                  padding:
+                      EdgeInsets.only(bottom: 80, right: 15, left: 15, top: 20),
+                  sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                          (context, index) => Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Post(FakeData.pub[index])),
+                          childCount: FakeData.pub.length))),
+            ],
           ),
-          SliverPadding(
-              padding:
-                  EdgeInsets.only(bottom: 80, right: 15, left: 15, top: 20),
-              sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                      (context, index) => Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Post(FakeData.pub[index])),
-                      childCount: FakeData.pub.length))),
-        ],
-      ),
-    );
+        ));
   }
 }
