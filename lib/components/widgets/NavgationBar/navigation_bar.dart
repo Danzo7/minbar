0bba20a8f23d80e47a1db869d7f0683538d29092
Danzo7 <aymenmiro@gmail.com>
2021-10-15 23:23:38 +0100

    cleanup

--- lib/components/widgets/NavgationBar/navigation_bar.dart
+++ lib/components/widgets/NavgationBar/navigation_bar.dart
@@ -4,6 +4,7 @@ import 'package:minbar_fl/components/screens/broadcast/Broadcast_page.dart';
 import 'package:minbar_fl/components/theme/default_theme.dart';
 
 import 'package:minbar_fl/components/widgets/buttons/buttons.dart';
+import 'package:minbar_fl/misc/page_navigation.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
@@ -21,42 +22,57 @@ class NavigationBar extends StatefulWidget {
   final NavType type;
   final int selectedIndex;
   final List<NavigatonItem> items;
-  final PageController? pageController;
-  const NavigationBar(
-      {Key? key,
-      this.type = NavType.idle,
-      required this.selectedIndex,
-      required this.items,
-      this.pageController})
-      : assert(items.length % 2 == 0),
+  const NavigationBar({
+    Key? key,
+    this.type = NavType.idle,
+    required this.selectedIndex,
+    required this.items,
+  })  : assert(items.length % 2 == 0),
         super(key: key);
 
   @override
-  _NavigationBarState createState() =>
-      _NavigationBarState(selectedIndex, pageController);
+  _NavigationBarState createState() => _NavigationBarState(selectedIndex);
 }
 
 class _NavigationBarState extends State<NavigationBar> {
-  final PageController? pageController;
   static final Widget listenButton = Container(
     alignment: Alignment.center,
     padding: const EdgeInsets.only(bottom: 30),
-    child: FlatIconButton(
-      backgroundColor: DColors.blueGray,
-      icon: Icon(SodaIcons.listening, size: 24, color: DColors.white),
-      onTap: () => {},
+    child: Column(
+      children: [
+        FlatIconButton(
+          backgroundColor: DColors.blueGray,
+          icon: Icon(SodaIcons.listening, size: 24, color: DColors.white),
+          onTap: () => {},
+        ),
+        Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          TextPlay(
+              textAlign: TextAlign.center,
+              minFontSize: 10,
+              marquee: Marquee(
+                showFadingOnlyWhenScrolling: true,
+                fadingEdgeEndFraction: 0.1,
+                text: "سوء الضنا الضنا الضنا",
+                style: TextStyle(color: DColors.white, fontSize: 12),
+                blankSpace: 50,
+                velocity: 20.0,
+              )),
+          Divider(
+            color: DColors.white,
+            thickness: 2,
+          )
+        ])
+      ],
     ),
   );
 
   int currentIndex;
-  _NavigationBarState(this.currentIndex, this.pageController);
+  _NavigationBarState(this.currentIndex);
   setBottomBarIndex(index) {
     if (currentIndex != index)
       setState(() {
-        if (pageController != null) {
-          pageController?.jumpToPage(index);
-        }
         currentIndex = index;
+        Pager.navigateTo(context, index);
       });
   }
 
@@ -80,8 +96,6 @@ class _NavigationBarState extends State<NavigationBar> {
                     ? broadcastPainter
                     : listenPainter,
           ),
-          if (widget.type == NavType.listen) listenButton,
-          if (widget.type == NavType.broadcastable) broadcastButton(context),
           Container(
             width: size.width,
             height: 80,
@@ -98,30 +112,6 @@ class _NavigationBarState extends State<NavigationBar> {
                             Container(
                               alignment: Alignment.bottomCenter,
                               width: size.width * 0.20,
-                              child: widget.type == NavType.listen
-                                  ? Column(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                          TextPlay(
-                                              textAlign: TextAlign.center,
-                                              minFontSize: 10,
-                                              marquee: Marquee(
-                                                showFadingOnlyWhenScrolling:
-                                                    true,
-                                                fadingEdgeEndFraction: 0.1,
-                                                text: "سوء الضنا الضنا الضنا",
-                                                style: TextStyle(
-                                                    color: DColors.white,
-                                                    fontSize: 12),
-                                                blankSpace: 50,
-                                                velocity: 20.0,
-                                              )),
-                                          Divider(
-                                            color: DColors.white,
-                                            thickness: 2,
-                                          )
-                                        ])
-                                  : null,
                             ),
                           IconButton(
                               icon: currentIndex == widget.items.indexOf(e)
@@ -130,8 +120,6 @@ class _NavigationBarState extends State<NavigationBar> {
                               onPressed: () {
                                 if (currentIndex != widget.items.indexOf(e)) {
                                   setBottomBarIndex(widget.items.indexOf(e));
-                                  if (pageController == null)
-                                    Navigator.popAndPushNamed(context, e.route);
                                 }
                               })
                         ])
@@ -141,47 +129,6 @@ class _NavigationBarState extends State<NavigationBar> {
             ),
           )
         ],
-      ),
-    );
-  }
-
-  Container broadcastButton(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.only(bottom: 20),
-      child: FlatIconButton(
-        icon: Icon(SodaIcons.broadcast, size: 24, color: DColors.white),
-        backgroundColor: DColors.sadRed,
-        onTap: () {
-          showMaterialModalBottomSheet(
-            expand: false,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-            ),
-            clipBehavior: Clip.antiAlias,
-            context: context,
-            backgroundColor: Colors.transparent,
-            builder: (context) => Container(
-                child: Container(
-              child: SlidingUpPanel(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.horizontal(
-                      left: Radius.circular(40), right: Radius.circular(40)),
-                  boxShadow: [],
-                  minHeight: 600,
-                  maxHeight: MediaQuery.of(context).size.height,
-                  panel: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(
-                          left: Radius.circular(40),
-                          right: Radius.circular(40)),
-                    ),
-                    child: BroadcastPage(600),
-                  )),
-            )),
-          );
-        },
       ),
     );
   }
