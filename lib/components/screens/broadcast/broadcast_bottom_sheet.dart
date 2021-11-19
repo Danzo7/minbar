e8d2f1d411ca1019ecc5f014ab0f8aaced4a074a
Danzo7 <aymenmiro@gmail.com>
2021-11-19 20:49:30 +0100

    fix dragController

--- lib/components/screens/broadcast/broadcast_bottom_sheet.dart
+++ lib/components/screens/broadcast/broadcast_bottom_sheet.dart
@@ -2,20 +2,29 @@ import 'package:minbar_fl/components/widgets/minbar_bottom_sheet.dart';
 import 'Broadcast_screen.dart';
 
 class BroadcastBottomSheet extends MinbarBottomSheet {
-  BroadcastBottomSheet(
+  BroadcastBottomSheet._(
       {required MinbarBottomSheetController controller,
       DragController? dragController})
       : super(
           radiusWhenNotExpanded: 44,
           allowSlideInExpanded: false,
           controller: controller,
-          dragController: dragController ?? DragController(),
+          dragController: dragController,
           closeWhenLoseFocus: true,
           child: BroadcastScreen(
             hasComments: true,
             controller: controller,
-            dragController: dragController ?? DragController(),
+            dragController: dragController,
           ),
           minHeight: 500,
         );
+
+  factory BroadcastBottomSheet(
+      {required MinbarBottomSheetController controller}) {
+    final DragController dragController = DragController();
+    return BroadcastBottomSheet._(
+      controller: controller,
+      dragController: dragController,
+    );
+  }
 }
