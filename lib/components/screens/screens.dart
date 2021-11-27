48abe70315c3befce73236e43aa519614ef62f84
Danzo7 <aymenmiro@gmail.com>
2021-11-27 16:00:16 +0100

    🎨 move screens to same folder

--- lib/components/screens/screens.dart
+++ lib/components/screens/screens.dart
@@ -1,3 +1,3 @@
 export 'login/login_screen.dart';
 export 'master_screen/master_screen.dart';
-export 'package:minbar_fl/components/settings/generated_settings/generated_settings_screen.dart';
+export 'generated_settings/generated_settings_screen.dart';
