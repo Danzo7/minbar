8a110a0f2de612f32c2b18a9d520a5ca487e7213
Danzo7 <aymenmiro@gmail.com>
2021-12-06 03:00:06 +0100

    🐛 Fix heart glitch

--- lib/components/common/timelines/posts_timeline/posts_repository.dart
+++ lib/components/common/timelines/posts_timeline/posts_repository.dart
@@ -7,6 +7,7 @@ import 'package:minbar_fl/model/publication.dart';
 
 class PostsRepository {
   final List<Publication> data = [];
+
   Future<List<Publication>> fetchItems() {
     return Future<List<Publication>>.delayed(
         Duration(seconds: Random().nextInt(3)), () {
@@ -20,7 +21,7 @@ class PostsRepository {
       {bool? liked, bool? pinned}) {
     return Future<bool>.delayed(Duration(seconds: 0), () {
       Publication res = doFakeUpdate(current, liked: liked, pinned: pinned);
-      if (res == current) return false;
+
       //TODO:catch exceptions
       data[data.indexWhere((element) => element.id == current.id)] = res;
       return true;
@@ -51,8 +52,9 @@ class PostsRepository {
                       ? -1
                       : 0)
           : 0;
+
       FakeData.pub[selectedIndex] = currentPub.copyWith(
-          hasHeart: liked ?? currentPub.hasCast,
+          hasHeart: liked ?? currentPub.hasHeart,
           hasPin: pinned ?? currentPub.hasPin,
           pinCount: currentPub.pinCount + pinAddition,
           heartCount: currentPub.heartCount + heartAddition);
