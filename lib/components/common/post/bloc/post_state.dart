d5a812817250c760c713cb9f3c69fe068053ab39
Danzo7 <aymenmiro@gmail.com>
2021-12-06 01:02:59 +0100

    🔥 dead code

--- lib/components/common/post/bloc/post_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'post_bloc.dart';
-
-@freezed
-class PostState with _$PostState {
-  const factory PostState.initial(Publication postData) = _Initial;
-  const factory PostState.inProgress() = _InProgress;
-  const factory PostState.done(Publication postData) = _Done;
-}
