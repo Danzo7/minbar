55a4a43e6ff545924e5d36d8061a04578bb2b45a
Danzo7 <aymenmiro@gmail.com>
2021-12-09 22:54:03 +0100

    ✨ Add snaping to middle floating button.

--- lib/components/widgets/NavgationBar/action_button.dart
+++ lib/components/widgets/NavgationBar/action_button.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:marquee/marquee.dart';
 import 'package:minbar_fl/components/theme/default_theme.dart';
 import 'package:minbar_fl/components/widgets/NavgationBar/middle_controller.dart';
+import 'package:minbar_fl/components/widgets/NavgationBar/floating_widget.dart';
 import 'package:minbar_fl/components/widgets/buttons/buttons.dart';
 import 'package:minbar_fl/components/widgets/misc/minbar_scaffold.dart';
-import 'package:minbar_fl/components/widgets/text_play.dart';
 import 'package:minbar_fl/components/widgets/voice_visualisation.dart';
 import 'package:minbar_fl/core/services/audio_service.dart';
 import 'package:minbar_fl/core/services/cast_service.dart';
 import 'package:minbar_fl/core/services/service_locator.dart';
 
 class ActionButton extends StatefulWidget {
-  final MiddleController middleController;
-
   ActionButton({
     Key? key,
     required this.state,
     required this.middleController,
   }) : super(key: key);
+
+  final MiddleController middleController;
   final CastService? state;
 
   @override
@@ -26,130 +25,75 @@ class ActionButton extends StatefulWidget {
 }
 
 class _ActionButtonState extends State<ActionButton> {
-  var isOverlay = false;
-  void showOverlay() => setState(() {
-        isOverlay = true;
-      });
-  void hideOverlay() => setState(() {
-        isOverlay = false;
-      });
   @override
   Widget build(BuildContext context) {
     bool isSwipeUp = false;
     void action() => showBroadcastBottomSheet(
           context,
         );
-
-    Widget dragablebubble(
-        {required Widget child,
-        required Widget broChild,
-        Function? onDragComplete}) {
-      return LongPressDraggable(
-        maxSimultaneousDrags: 1,
-        ignoringFeedbackSemantics: false,
-        axis: Axis.vertical,
-        child:
-            Wrap(alignment: WrapAlignment.center, children: [child, broChild]),
-        feedback: child,
-        onDragStarted: () {
-          widget.middleController.normal();
-          showOverlay();
-        },
-        onDragEnd: (details) {
-          if (MediaQuery.of(context).size.height - 150 > details.offset.dy) {
-            if (onDragComplete != null) onDragComplete();
-          } else {
-            widget.middleController.outside();
-          }
-          hideOverlay();
-        },
-        onDragUpdate: (details) {},
-        childWhenDragging: SizedBox(),
-      );
-    }
-
     return (widget.state == null || widget.state?.currentCast == null)
         ? SizedBox()
-        : Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              isOverlay
-                  ? Material(
-                      color: Colors.transparent,
-                      elevation: 1,
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 100),
-                              child: Center(
-                                child: ClipOval(
-                                    child: Container(
-                                  color: DColors.sadRed,
-                                  height: 70,
-                                  width: 70,
-                                  child:
-                                      Center(child: Icon(Icons.stop_rounded)),
-                                )),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                  : SizedBox(),
-              GestureDetector(
-                onVerticalDragUpdate: (details) =>
-                    isSwipeUp = details.delta.dy < 0 ? true : false,
-                onVerticalDragEnd: (c) => {if (isSwipeUp) action()},
-                child: Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 101,
-                  width: MediaQuery.of(context).size.width / 5,
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: StreamBuilder<PlayerState>(
-                      stream: app<AudioService>().playerStateStream,
-                      builder: (BuildContext context,
-                          AsyncSnapshot<PlayerState> snapshot) {
-                        final playerState = snapshot.data;
-                        final processingState = playerState?.processingState;
-
-                        return dragablebubble(
-                            onDragComplete: widget.state!.stopCast,
-                            child: FlatIconButton(
-                                backgroundColor: minbarTheme.secondary,
-                                icon: (processingState ==
-                                            ProcessingState.ready &&
-                                        app<AudioService>().playerState.playing)
-                                    ? VoiceVisualisation()
-                                    : (processingState ==
-                                                ProcessingState.buffering ||
-                                            processingState ==
-                                                ProcessingState.loading)
-                                        ? CircularProgressIndicator()
-                                        : Icon(
-                                            Icons.pause,
-                                            color: DColors.white,
-                                          ),
-                                onTap: action),
-                            broChild: TextPlay(
-                                textAlign: TextAlign.center,
-                                minFontSize: 10,
-                                marquee: Marquee(
-                                  showFadingOnlyWhenScrolling: true,
-                                  fadingEdgeEndFraction: 0.1,
-                                  text: widget.state!.currentCast!.subject,
-                                  style: DTextStyle.w12,
-                                  blankSpace: 50,
-                                  velocity: 20.0,
-                                )));
-                      }),
-                ),
-              ),
+        : FloatingWidget(
+            onDragCanceled: () {
+              widget.middleController.inside();
+            },
+            withHitDetection: true,
+            padding: EdgeInsets.only(bottom: 30),
+            snap: [
+              SnapTap(
+                  pos: Offset(0, -100),
+                  radius: 50,
+                  onSnap: () => widget.state!.stopCast(),
+                  snapLocator: ClipOval(
+                      child: Container(
+                    color: DColors.sadRed,
+                    height: 70,
+                    width: 70,
+                    child: Center(
+                        child: Icon(
+                      Icons.stop_rounded,
+                    )),
+                  ))),
+              HitSnapTap(
+                pos: Offset.zero,
+                radius: 50,
+                onHitChange: (bool isIn) => isIn
+                    ? widget.middleController.inside()
+                    : widget.middleController.normal(),
+              )
             ],
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) =>
+                  isSwipeUp = details.delta.dy < 0 ? true : false,
+              onVerticalDragEnd: (c) => {if (isSwipeUp) action()},
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                height: 101,
+                width: MediaQuery.of(context).size.width / 5,
+                child: StreamBuilder<PlayerState>(
+                    stream: app<AudioService>().playerStateStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<PlayerState> snapshot) {
+                      final playerState = snapshot.data;
+                      final processingState = playerState?.processingState;
+
+                      return FlatIconButton(
+                          backgroundColor: minbarTheme.secondary,
+                          icon: (processingState == ProcessingState.ready &&
+                                  app<AudioService>().playerState.playing)
+                              ? VoiceVisualisation()
+                              : (processingState == ProcessingState.buffering ||
+                                      processingState ==
+                                          ProcessingState.loading)
+                                  ? CircularProgressIndicator()
+                                  : Icon(
+                                      Icons.pause,
+                                      color: DColors.white,
+                                    ),
+                          onTap: action);
+                    }),
+              ),
+            ),
           );
   }
 }
--- lib/components/widgets/NavgationBar/navigation_bar.dart
+++ lib/components/widgets/NavgationBar/navigation_bar.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:marquee/marquee.dart';
+import 'package:minbar_fl/components/theme/default_text_styles.dart';
 import 'package:minbar_fl/components/theme/minbar_theme.dart';
 import 'package:minbar_fl/components/widgets/NavgationBar/animated_painterd.dart';
 import 'package:minbar_fl/components/widgets/NavgationBar/middle_controller.dart';
 import 'package:minbar_fl/components/widgets/NavgationBar/navigation_item.dart';
+import 'package:minbar_fl/components/widgets/text_play.dart';
+import 'package:minbar_fl/core/services/cast_service.dart';
 import 'package:minbar_fl/core/services/service_locator.dart';
 import 'package:minbar_fl/misc/navigation.dart';
 import 'package:minbar_fl/misc/page_navigation.dart';
+import 'package:provider/provider.dart';
 
 class NavigationBar extends StatefulWidget {
-  final MiddleController middleController;
-  final int selectedIndex;
-  final List<NavigatonItem> items;
-  final NavgationController? navigationController;
-
   const NavigationBar({
     Key? key,
     required this.selectedIndex,
@@ -23,14 +23,29 @@ class NavigationBar extends StatefulWidget {
   })  : assert(items.length % 2 == 0),
         super(key: key);
 
+  final List<NavigatonItem> items;
+  final MiddleController middleController;
+  final NavgationController? navigationController;
+  final int selectedIndex;
+
   @override
   _NavigationBarState createState() => _NavigationBarState();
+
+  get state => null;
 }
 
 class _NavigationBarState extends State<NavigationBar> {
   int currentIndex = 0;
   AnimationDirection mad = AnimationDirection.idle;
   late final NavgationController? navigationController;
+
+  @override
+  void didUpdateWidget(covariant NavigationBar oldWidget) {
+    navigationController?.addChangeListener(setBottomBarIndex);
+
+    widget.middleController.addListener(listener);
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   void initState() {
@@ -43,14 +58,6 @@ class _NavigationBarState extends State<NavigationBar> {
     super.initState();
 
     navigationController?.addChangeListener(setBottomBarIndex);
-  }
-
-  @override
-  void didUpdateWidget(covariant NavigationBar oldWidget) {
-    navigationController?.addChangeListener(setBottomBarIndex);
-
-    widget.middleController.addListener(listener);
-    super.didUpdateWidget(oldWidget);
   }
 
   void listener() async {
@@ -95,7 +102,7 @@ class _NavigationBarState extends State<NavigationBar> {
           alignment: Alignment.bottomCenter,
           clipBehavior: Clip.none,
           children: [
-            AnimatedPainter(direction: mad, isInside: false, max: 15),
+            AnimatedPainter(direction: mad, isInside: false, max: 12),
             Container(
               width: size.width,
               height: 80,
@@ -109,12 +116,33 @@ class _NavigationBarState extends State<NavigationBar> {
                             if (widget.items.indexOf(e) ==
                                 widget.items.length / 2)
                               AnimatedContainer(
-                                duration: kShortAnimationDuration,
-                                alignment: Alignment.bottomCenter,
-                                width: widget.middleController.isNormal
-                                    ? 0
-                                    : (size.width * 0.20),
-                              ),
+                                  duration: kShortAnimationDuration,
+                                  alignment: Alignment.bottomCenter,
+                                  width: widget.middleController.isNormal
+                                      ? 0
+                                      : (size.width * 0.20),
+                                  child: Consumer<CastService>(
+                                      builder: (context, state, child) {
+                                    return state.currentCast != null
+                                        ? Padding(
+                                            padding: const EdgeInsets.only(
+                                                bottom: 8.0),
+                                            child: TextPlay(
+                                                textAlign: TextAlign.center,
+                                                minFontSize: 10,
+                                                marquee: Marquee(
+                                                  showFadingOnlyWhenScrolling:
+                                                      true,
+                                                  fadingEdgeEndFraction: 0.1,
+                                                  text: state
+                                                      .currentCast!.subject,
+                                                  style: DTextStyle.w12,
+                                                  blankSpace: 50,
+                                                  velocity: 20.0,
+                                                )),
+                                          )
+                                        : SizedBox();
+                                  })),
                             IconButton(
                                 icon: currentIndex == widget.items.indexOf(e)
                                     ? e.beforeIcon
